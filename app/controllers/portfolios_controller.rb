ecec3ec6ce36679064bb29519d89dbae5d6e0cdf
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-04-14 16:26:31 -0600

    Added edit and update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,16 +8,40 @@ class PortfoliosController < ApplicationController
 	@portfolio_item = Portfolio.new
   end
 
+# Creates portfolio items
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
-        end
+       end
+     end
+   end
+# GET /portfolio
+   
+   def edit
+    	@portfolio_item = Portfolio.find(params[:id])
+   end
+
+def update
+	@portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
+  end
+
+  def show
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+  
+	
+
 
 end
