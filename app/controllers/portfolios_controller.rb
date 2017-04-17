168367bb24dad12e3553657f92a814e6234d5c3c
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-04-17 11:15:20 -0600

    Destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,6 +43,22 @@ def update
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+def destroy
+  # Performe the look up
+  @portfolio_item = Portfolio.find(params[:id])
+
+# Destroys/delete the record
+    @portfolio_item.destroy
+
+    #Redirect 
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
+    end
+  end
+
+
+
+
 	end
 
 
