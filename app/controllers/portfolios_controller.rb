49e8575c31cbdc6158a1e58a2e394b3b4645b089
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-05-01 15:17:48 -0600

    Updated fix for show page image and added edit button

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,7 +21,6 @@ class PortfoliosController < ApplicationController
 
   def new
 	  @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end
 
 # Creates portfolio items
@@ -74,7 +73,7 @@ end
                                       :body,
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name]
+                                      technologies_attributes: [:id, :name, :_destroy]
                                       )
   end
 
